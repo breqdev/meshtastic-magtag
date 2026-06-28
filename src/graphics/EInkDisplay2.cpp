7b11cdaa163e0024dac6b4dca3503799597e3f35
Brooke Chalmers <breq@breq.dev>
2026-06-28 16:34:56 -0400

    hack to change rotation (should be moved to a parameter)

--- src/graphics/EInkDisplay2.cpp
+++ src/graphics/EInkDisplay2.cpp
@@ -254,7 +254,7 @@ bool EInkDisplay::connect()
         auto lowLevel = new EINK_DISPLAY_MODEL(PIN_EINK_CS, PIN_EINK_DC, PIN_EINK_RES, PIN_EINK_BUSY);
         adafruitDisplay = new GxEPD2_BW<EINK_DISPLAY_MODEL, EINK_DISPLAY_MODEL::HEIGHT>(*lowLevel);
         adafruitDisplay->init(115200, true, 40, false, SPI, SPISettings(4000000, MSBFIRST, SPI_MODE0));
-        adafruitDisplay->setRotation(1);
+        adafruitDisplay->setRotation(3);
         adafruitDisplay->setPartialWindow(0, 0, EINK_WIDTH, EINK_HEIGHT);
     }
 #elif defined(HELTEC_MESH_POCKET) || defined(SEEED_WIO_TRACKER_L1_EINK) || defined(HELTEC_MESH_SOLAR_EINK)
